d2266bc8c9f23ce16af9e0df425b2ba93547fa56
Keith Jerosky <kjerosky@gmail.com>
2023-03-02 23:12:09 -0700

    Initial commit - glfw opengl 3.3 window displaying imgui demo

---
+++ Makefile
@@ -0,0 +1,32 @@
+EXECUTABLE = mathimate
+
+IMGUI_DIRECTORY = imgui
+IMGUI_SOURCE_FILES = \
+	${IMGUI_DIRECTORY}/imgui_demo.cpp \
+	${IMGUI_DIRECTORY}/imgui_draw.cpp \
+	${IMGUI_DIRECTORY}/imgui_tables.cpp \
+	${IMGUI_DIRECTORY}/imgui_widgets.cpp \
+	${IMGUI_DIRECTORY}/imgui.cpp \
+	${IMGUI_DIRECTORY}/imgui_impl_opengl3.cpp \
+	${IMGUI_DIRECTORY}/imgui_impl_glfw.cpp
+
+INCLUDE_PATHS = \
+	-I/opt/homebrew/opt/glfw/include \
+	-I/opt/homebrew/opt/glew/include \
+	-I/opt/homebrew/opt/ffmpeg/include \
+	-I${IMGUI_DIRECTORY}
+
+LIBRARY_PATHS = \
+	-L/opt/homebrew/opt/glfw/lib \
+	-L/opt/homebrew/opt/glew/lib \
+	-L/opt/homebrew/opt/ffmpeg/lib
+
+LIBRARIES = -framework OpenGL -lglfw -lglew -lavcodec -lavutil
+
+SOURCE_FILES = main.cpp
+
+all:
+	g++ -o ${EXECUTABLE} -std=c++11 ${IMGUI_SOURCE_FILES} ${SOURCE_FILES} ${INCLUDE_PATHS} ${LIBRARY_PATHS} ${LIBRARIES}
+
+clean:
+	rm -rf ${EXECUTABLE}
---
+++ main.cpp
@@ -0,0 +1,86 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#include <GLFW/glfw3.h>
+
+static void glfw_error_callback(int error, const char* description) {
+    std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+int main(int argc, char *argv[]) {
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+        return 1;
+    }
+
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_MAXIMIZED, GL_TRUE);
+
+    // Create window with graphics context
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "Mathimate", NULL, NULL);
+    if (window == NULL) {
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    while (!glfwWindowShouldClose(window)) {
+        // Poll and handle events (inputs, window resize, etc.)
+        // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
+        // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
+        // - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
+        // Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+        glfwPollEvents();
+
+        // Start the Dear ImGui frame
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+        ImGui::ShowDemoWindow();
+
+        // Rendering
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(0.45f, 0.55f, 0.6f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
